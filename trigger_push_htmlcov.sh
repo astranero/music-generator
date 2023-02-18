fd4ac2eb92a0626e8bec5672110a49792aef1d60
astranero <esa.taheresa@gmail.com>
2023-02-18 13:56:13 +0200

    msg=test

--- trigger_push_htmlcov.sh
+++ trigger_push_htmlcov.sh
@@ -6,8 +6,8 @@ git add htmlcov/.
 git commit -m "adding 'htmlcov' directory from 'main' branch to the GitHubs Actions." 
 git push origin html-coverage
 git checkout main
-git rm -rf htmlcov/*
 git rm -rf htmlcov
+git rmdir -rf htmlcov
 git add .
 git commit -m "Remove unnecessary htmlcov files from the 'main' branch."
 git push origin main
