25292ae4b8f339840e97c12d28f16564d43f81ca
astranero <esa.taheresa@gmail.com>
2023-02-18 17:42:22 +0200

    adding 'htmlcov' directory from 'main' branch to the GitHubs Actions.

--- trigger_push_htmlcov.sh
+++ trigger_push_htmlcov.sh
@@ -5,11 +5,7 @@ git checkout html-coverage
 git checkout main -- Documentation/
 
 echo $pwd
-for file in $pwd
-do 
-    cp $file ./
-    echo $file
-done
+
 
 git add .
 git commit -m "adding 'htmlcov' directory from 'main' branch to the GitHubs Actions." 
