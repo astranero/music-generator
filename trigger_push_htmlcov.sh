d8fcf70063a6aac44d888f46f1206dc534910733
astranero <esa.taheresa@gmail.com>
2023-02-18 16:59:07 +0200

    Add a file copy to trigger

--- trigger_push_htmlcov.sh
+++ trigger_push_htmlcov.sh
@@ -4,6 +4,12 @@ git switch html-coverage
 git show main:htmlcov > .
 git show main:.coverage > .
 
+for file in htmlcov
+do 
+    cp file .
+done
+
+
 git add .
 git commit -m "adding 'htmlcov' directory from 'main' branch to the GitHubs Actions." 
 git push origin html-coverage
