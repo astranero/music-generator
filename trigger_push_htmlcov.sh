5d7d2c931522b33c77e2d324a42020eee5c20783
astranero <esa.taheresa@gmail.com>
2023-02-18 15:46:55 +0200

    msg=test

--- trigger_push_htmlcov.sh
+++ trigger_push_htmlcov.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 git checkout html-coverage
-git checkout main -- htmlcov/
+git checkout main -- htmlcov
 
-git add htmlcov/ 
+git add htmlcov
 git commit -m "adding 'htmlcov' directory from 'main' branch to the GitHubs Actions." 
 git push origin html-coverage
 
