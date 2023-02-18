33c5792aed39fedf8088b0aa59685e61ad50c697
astranero <esa.taheresa@gmail.com>
2023-02-18 17:45:14 +0200

    test

--- tasks.py
+++ tasks.py
@@ -14,11 +14,8 @@ def start(ctx):
 
 @task
 def coverage_report(ctx):
-    ctx.run("git stash")
     ctx.run("poetry run pytest --cov-report html --cov=src", pty=True)
-    ctx.run("chmod u+x trigger_push_htmlcov.sh", pty=True)
-    ctx.run("bash trigger_push_htmlcov.sh", pty=True)
-    time.sleep(2)
+   
 
 
 @task
@@ -26,7 +23,10 @@ def push(ctx, msg):
     ctx.run("git add .", pty=True)
     ctx.run(f"git commit -m {msg}")
     ctx.run("git push origin main", pty=True)
-    coverage_report(ctx)
+    ctx.run("git stash")
+    ctx.run("poetry run pytest --cov-report html --cov=src", pty=True)
+    ctx.run("chmod u+x trigger_push_htmlcov.sh", pty=True)
+    ctx.run("bash trigger_push_htmlcov.sh", pty=True)
 
 
 @task
